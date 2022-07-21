40606d2be5571e4c54b7fb75f509b0adcb10bf7d
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-07-20 23:00:15 -0600

    add install Redux-toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+import { useSelector } from 'react-redux';
+
+// useDispatch = is to do something | It will do the functions that we want to call in order to update the state.
+// useSelector = it is to select or bring something | the way in which we are going to be able to bring the data that is within the state.
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const taskState = useSelector(state => state.tasks);
+  console.log(taskState)
+
+
 
   return (
     <div className="App">
-      
+      Hello World
     </div>
   )
 }
---
+++ src/features/tasks/taskSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+
+export const taskSlice = createSlice({
+    name: 'tasks',
+    initialState: [],
+    reducers:{
+
+    }
+})
+
+export default taskSlice.reducer
